3254723a3c5b43c35bf76880691ea619f296eb55
Ashish Jain <ashishjain4365@gmail.com>
2022-03-28 00:36:21 +0530

    ITT LAB

--- Sem6/ITT LAB/LAB3/Q2/Q2.js
+++ Sem6/ITT LAB/LAB3/Q2/Q2.js
@@ -1,23 +1,19 @@
-var text = document.getElementById("Q2").innerHTML;
-var a=text.split(" ");
-var s="";
-var n = a.length;
-var i;
-var txt;
-document.getElementById("Q2").textContent = "";
-function piglatin(text){
-    for(i=0;i<n;i++)
-    {
-        txt=a[i];
-        var s="";
-        s=txt.substring(1,txt.length);
-        s+=txt.charAt(0);
-        s+="ay";
-        var temp = document.getElementById("Q2").innerHTML;
-        s = temp + " " + s;
-        document.getElementById("Q2").textContent = s;
-        setTimeout(100);
-    }
+function piglatin(text) {
+  var text = document.getElementById("Q2").value;
+  var a = text.split(" ");
+  var s = "";
+  var n = a.length;
+  var i;
+  var txt;
+  document.getElementById("Q2").textContent = "";
+  for (i = 0; i < n; i++) {
+    txt = a[i];
+    var s = "";
+    s = txt.substring(1, txt.length);
+    s += txt.charAt(0);
+    s += "ay";
+    var temp = document.getElementById("display").innerHTML;
+    s = temp + " " + s;
+    document.getElementById("display").textContent = s;
+  }
 }
-
-piglatin();
