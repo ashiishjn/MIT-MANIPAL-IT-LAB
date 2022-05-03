60040d7e87b0287374096702f31292d76daa835c
Ashish Jain <ashishjain4365@gmail.com>
2022-05-03 20:10:56 +0530

    Updated codes

--- Sem6/ITT LAB/LAB6/1d.py
+++ Sem6/ITT LAB/LAB6/1d.py
@@ -1,7 +1,8 @@
-with open("names.txt", 'r') as names_file:
-    with open("body.txt", 'r') as body_file:
-        body = body_file.read()
-        for name in names_file:
-            mail = "Hello " + name.strip() + "\n" + body
-            with open(name.strip()+".txt", 'w') as mail_file:
-                mail_file.write(mail)
+body = open("body.txt", "r+")
+name = open("names.txt", "r+")
+bdy = body.read()
+for nam in name:
+  print(nam)
+  mail  = "Hello " + nam.strip() + "\n" + bdy
+  wri = open(nam.strip()+".txt", "w")
+  wri.write(mail)
